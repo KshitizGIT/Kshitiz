a0246226a885d1e4a6ed22ae87024971737d9d9f
Kshitiz Shakya <kshakya@growbydata.com>
2020-07-15 23:34:15 +0545

    Adding resources for styles and javascript

---
+++ TheRonaldoTheme/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+using OrchardCore.ResourceManagement;
+
+namespace TheRonaldoTheme
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IResourceManifestProvider, ResourceManifest>();
+        }
+    }
+}
